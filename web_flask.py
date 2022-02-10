40464905e40cf832af589b816d3fca95eafb87bb
kirussian <python.monaco@yandex.ru>
2022-02-10 19:15:06 +0400

    test commit

---
+++ web_flask.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import requests
+from bs4 import BeautifulSoup as bs
+
+app = Flask(__name__)
+
+
+@app.route('/live/', methods=['GET'])
+def html():
+    url = 'https://football.kulichki.net/live.htm'
+    html_response = requests.get(url).content
+    soup = bs(html_response, 'html.parser').find('table')
+    return str(soup)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
