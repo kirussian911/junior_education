bf288168864fe3b627f5713b29697805072e8825
kirussian <python.monaco@yandex.ru>
2022-02-10 23:24:52 +0400

    add prognoz

--- web_flask.py
+++ web_flask.py
@@ -6,10 +6,18 @@ app = Flask(__name__)
 
 
 @app.route('/live/', methods=['GET'])
-def html():
+def get_live_result():
     url = 'https://football.kulichki.net/live.htm'
     html_response = requests.get(url).content
     soup = bs(html_response, 'html.parser').find('table')
+    return str(soup)
+
+
+@app.route('/prognoz/', methods=['GET'])
+def get_probability():
+    url = 'https://football.kulichki.net/prognoz/'
+    html_response = requests.get(url).content
+    soup = bs(html_response, 'html.parser').find(class_='col2 inl vtop')
     return str(soup)
 
 
